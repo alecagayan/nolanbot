5c5f3a5afd163615283b373d4ab3c64c5a0fa462
oopsie1412 <alecagayan24@gmail.com>
2021-03-10 20:56:42 -0500

    Update welcomer.py

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -9,19 +9,13 @@ class Welcomer(commands.Cog):
     async def on_member_update(self, before, after):
 
         print('update: ' + after.display_name)
+        print()
 
         print(before.roles)
         print(after.roles)
         #if((any(role.name == 'Youtube Member' for role in after.roles) and any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) and not any(role.name == 'Boost Creeps' for role in after.roles)) and len(after.roles) > len(before.roles)):
         #if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) or (any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) and not any(role.name == 'Youtube Member' for role in after.roles))) and ((len(after.roles) > len(before.roles)) and not any(role.name == 'Boost Creeps' for role in after.roles)) ):
-        if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member: Donut Underground' for role in after.roles)) and (len(after.roles) > len(before.roles))):
-            channel = self.bot.get_channel(741398310228721844)
-            guild = before.guild
-            print('member! ' + after.display_name)
-
-            to_send = 'Welcome to the underground, {0.mention}.'.format(before, guild)
-            await channel.send(to_send)
-        elif((any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) and not any(role.name == 'Youtube Member' for role in after.roles)) and (len(after.roles) > len(before.roles))):
+        if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member' for role in before.roles)) and (len(after.roles) > len(before.roles))):
             channel = self.bot.get_channel(741398310228721844)
             guild = before.guild
             print('member! ' + after.display_name)
