02c32ca4f36129a8f23330aa1f75363b04455436
oopsie1412 <alecagayan24@gmail.com>
2021-03-11 14:01:43 -0500

    Update welcomer.py

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -5,22 +5,30 @@ from discord.ext import commands
 class Welcomer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
+    async def on_member_join(self, member):
+        channel = self.bot.get_channel(741398310228721844)
+        guild = member.guild
 
-        print('update: ' + after.display_name)
+        to_send = 'Welcome to the underground, {0.mention}.'.format(member, guild)
+        await channel.send(to_send)
 
-        print(before.roles)
-        print(after.roles)
+    #sync def on_member_update(self, before, after):
+#
+#        print('update: ' + after.display_name)
+#
+#        print(before.roles)
+#        print(after.roles)
         #if((any(role.name == 'Youtube Member' for role in after.roles) and any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) and not any(role.name == 'Boost Creeps' for role in after.roles)) and len(after.roles) > len(before.roles)):
         #if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) or (any(role.name == 'Youtube Member: Donut Underground' for role in after.roles) and not any(role.name == 'Youtube Member' for role in after.roles))) and ((len(after.roles) > len(before.roles)) and not any(role.name == 'Boost Creeps' for role in after.roles)) ):
-        if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member' for role in before.roles)) and (len(after.roles) > len(before.roles))):
-            channel = self.bot.get_channel(741398310228721844)
-            guild = before.guild
-            print('member! ' + after.display_name)
-
-            to_send = 'Welcome to the underground, {0.mention}.'.format(before, guild)
-            await channel.send(to_send)
+#        if((any(role.name == 'Youtube Member' for role in after.roles) and not any(role.name == 'Youtube Member' for role in before.roles)) and (len(after.roles) > len(before.roles))):
+#            channel = self.bot.get_channel(741398310228721844)
+#            guild = before.guild
+#            print('member! ' + after.display_name)
+#
+#            to_send = 'Welcome to the underground, {0.mention}.'.format(before, guild)
+#            await channel.send(to_send)
 
 def setup(bot):
     bot.add_cog(Welcomer(bot))
