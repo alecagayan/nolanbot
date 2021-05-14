aceb5946fdc0f2a5b9bb56a3ddc6d421c4c3c508
alec <alecagayan24@gmail.com>
2021-05-14 18:09:14 -0400

    Update welcomer.py

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -22,7 +22,7 @@ class Welcomer(commands.Cog):
             channel = self.bot.get_channel(741398310228721844)
             guild = member.guild
 
-            to_send = 'Welcome to the underground, {0.mention}.'.format(member, guild)
+            to_send = 'Welcome to the underground, {0.mention}.'.format(member)
             await channel.send(to_send)
             await channel.send("Rule 1: welcome into the server, I hope you have a wonderful time in here! Please see <#731209804504367104> for the official server rules\nRule 2: if you would like to, please show us your car in <#733836074278846514>! All cars are welcome!")
             await channel.send("Rule 3: please show us your pets! You can send pictures of your wonderful animal friends in <#734487049364832350>! p.s. I would die for them\nRule 4: Make sure to head over to <#748994641721950288> and use the **!dbhelp** command to add your car and pet to the Underground database!\n-DogeLord1998")
