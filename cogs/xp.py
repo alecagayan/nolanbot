75588dd1af97fdf0afeb734cad9612df88e8d418
alec <alecagayan24@gmail.com>
2021-06-06 12:29:30 -0400

    Update xp.py

--- cogs/xp.py
+++ cogs/xp.py
@@ -14,9 +14,6 @@ class Xp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    print("hi2")
-
-
     db = connect(DB_PATH, check_same_thread=False)
     cur = db.cursor()
 
@@ -47,20 +44,9 @@ class Xp(commands.Cog):
             lvl = cur.fetchone()[0]
         else:
             lvl = 0
-        cur.execute(f"SELECT XPLock FROM xp WHERE UserID = {message.author.id}")
-        if cur.fetchone() is not None:
-            xplock = cur.fetchone()[0]
-        else:
-            xplock = ((datetime.utcnow()+timedelta(seconds=60)).isoformat())
 
-        print(xp)
-        print(lvl)
-        print(xplock)
-
-        if datetime.utcnow() > datetime.fromisoformat(xplock):
-            await self.add_xp(message, xp, lvl)
-
-
+        #if datetime.utcnow() > datetime.fromisoformat(xplock):
+        await self.add_xp(message, xp, lvl)
 
     async def add_xp(self, message, xp, lvl):
         xp_to_add = randint(10, 20)
@@ -68,9 +54,7 @@ class Xp(commands.Cog):
             xp = 0
         if lvl == None:
             lvl = 0
-        print("")
-        print(xp)
-        print(lvl)
+
         new_lvl = int(((int(xp)+xp_to_add)//42) ** 0.55)
 
         db = connect(DB_PATH, check_same_thread=False)
@@ -79,11 +63,11 @@ class Xp(commands.Cog):
         cur.execute(f"SELECT XP FROM xp WHERE UserID = {message.author.id}")
 
         check = cur.fetchone()
-        print(check)
 
         if check is None:
-            print("inserting")
-            cur.execute("INSERT INTO xp(UserID, XP) VALUES(?,?)", (message.author.id, 0))
+
+            cur.execute("INSERT INTO xp(UserID, XP) VALUES(?,?)",
+                        (message.author.id, 0))
 
         cur.execute(
             "UPDATE xp SET XP = ? WHERE UserID = ?", (int(xp) + xp_to_add, message.author.id))
@@ -96,14 +80,23 @@ class Xp(commands.Cog):
         cur.close()
         db.close()
 
-
-
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
-            print("debug1")
             await self.process_xp(message)
 
+    @commands.command()
+    async def xp(self, ctx):
+        db = connect(DB_PATH, check_same_thread=False)
+        cur = db.cursor()
+
+        cur.execute(f"SELECT * FROM xp WHERE UserID = {ctx.author.id}")
+        res = cur.fetchall()
+        print(res)
+        await ctx.send(str(res))
+
+        cur.close()
+        db.close()
 
 def setup(bot):
     bot.add_cog(Xp(bot))
