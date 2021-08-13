676a3d1c1789f3025f600619b1fd223309031869
alec <alecagayan24@gmail.com>
2021-08-12 23:33:11 -0400

    typo and embed fix

--- cogs/dm.py
+++ cogs/dm.py
@@ -16,7 +16,6 @@ class Dm(commands.Cog):
             return m.author == message.author
 
         if not message.guild and not message.author.bot:
-            print('msg: ' + message.content)
 
             exclude = ['Y', 'y', 'N', 'n', '1', '2']
 
@@ -27,25 +26,24 @@ class Dm(commands.Cog):
             confirm = await self.bot.wait_for('message', check=check)
             confirmcontent = confirm.content
 
-            if confirmcontent is "Y" or confirmcontent is "y":
+            if confirmcontent == "Y" or confirmcontent == "y":
                 await message.channel.send("Sounds good! Would you like to file your report anonymously **(`1`)** or not **(`2`)**? *If not, only server mods will know who you are.* This lets them reach out to you in the future!")
 
                 anon = await self.bot.wait_for('message', check=check)
                 anoncontent = anon.content
 
-                if anoncontent is "1":
+                if anoncontent == "1":
                     user = "Anonymous"
-                    print('usr: ' + user)
 
                     embed = discord.Embed(color=0xED4245) #Red
                     embed.add_field(name=user + ' has filed a report!', value=report, inline=True)
                     embed.set_footer(text='Filed on ' + str(datetime.datetime.now()))
                     await reportchannel.send(embed=embed)
                     
-                    await message.channel.send("Anonymous eport sent!")
+                    await message.channel.send("Anonymous report sent!")
 
-                if anoncontent is "2":
-                    user = message.author.display_name + message.author.discriminator
+                if anoncontent == "2":
+                    user = message.author.display_name + '#' + message.author.discriminator
 
                     embed = discord.Embed(color=0xED4245) #Red
                     embed.add_field(name=user + ' has filed a report!', value=report, inline=True)
@@ -54,7 +52,7 @@ class Dm(commands.Cog):
 
                     await message.channel.send("Report sent!")
 
-            if confirmcontent is "N" or confirmcontent is "n":
+            if confirmcontent == "N" or confirmcontent == "n":
                 await message.channel.send("Your report has been *cancelled*!")
 
 
