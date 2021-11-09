233e5ee686692b58bc7b6751736179d109b54917
alec <alecagayan24@gmail.com>
2021-11-08 19:39:17 -0500

    Update welcomer.py

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -20,7 +20,7 @@ class Welcomer(commands.Cog):
             print('role found')
             channel = self.bot.get_channel(741398310228721844)
 
-            to_send = 'Welcome to the underground, {0.mention}!'.format(member)
+            to_send = '**Welcome to the underground, {0.mention}!**'.format(member)
 
             await channel.send("Please read the <#731209804504367104> before continuing. <#788857573230772294> is a great resource if you have any questions. Otherwise, all mods have pink names and would be happy to help you out!\n\nPosting your car to <#733836074278846514> or your animal friend to <#734487049364832350> would make for a great introduction! If you’d like to add them to our database, head over to <#748994641721950288> and type `!dbhelp` to get started\n\nEnjoy your stay, and remember – be kind!")
             await channel.send(to_send)
