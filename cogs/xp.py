5fed5577482d486209adbe6a17e9938b13fc5bd9
alec <alecagayan24@gmail.com>
2021-11-24 10:58:07 -0500

    Update xp.py

--- cogs/xp.py
+++ cogs/xp.py
@@ -128,7 +128,7 @@ class Xp(commands.Cog):
         db = connect(DB_PATH, check_same_thread=False)
         cur = db.cursor()
 
-        cur.execute("SELECT * FROM xp ORDER BY XP DESC LIMIT 10")
+        cur.execute("SELECT * FROM xp ORDER BY XP DESC")
         res = cur.fetchall()
 
         await ctx.send(res)
